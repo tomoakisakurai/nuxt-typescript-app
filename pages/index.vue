fcb3ee75473800ff1c257a7acea3a2f1a5011e16
Tomoaki Sakurai <ssaku.power@gmail.com>
2020-05-04 01:16:37 +0900

    Modify text

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxt-sample
       </h1>
       <h2 class="subtitle">
-        This is a nuxt-typescript-project.
+        これはnuxtのテストです。netlify にデプロイしたよ。
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">
